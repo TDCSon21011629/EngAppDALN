8c2938c3df1e7d07cc6b54cb40fa715c5377352b
TDCSon <97428357+TDCSon21011629@users.noreply.github.com>
2024-07-19 16:27:01 +0700

    Update main.dart

--- eng_app/lib/main.dart
+++ eng_app/lib/main.dart
@@ -1,58 +1,58 @@
 import 'dart:io';
-import 'package:eng_app/screens/main_page.dart';
-import 'package:eng_app/screens/translator_screen.dart';
+
+
 import 'package:eng_app/screens/Setting.dart';
-import 'package:eng_app/screens/home_screen.dart';
+import 'package:eng_app/screens/home_login_screen.dart';
 import 'package:eng_app/screens/login.dart';
-import 'package:eng_app/screens/search_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:eng_app/screens/welcome_screen.dart';
 import 'package:eng_app/config/app_theme.dart';
+import 'package:eng_app/screens/translator_screen.dart';
+import 'package:eng_app/screens/search_page.dart';
+import 'package:eng_app/screens/home-screen/home_screen.dart';
 
 
 
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Platform.isAndroid ?
-    await Firebase.initializeApp(
-      options:FirebaseOptions(
-          apiKey: "AIzaSyD_I8LTksveghTmTCDbAB8YHX8j0MCIryI",
-          appId: "1:714749532013:android:175644c4d9e4237d6cac64",
-          messagingSenderId: "714749532013",
-          projectId: "english-vocabulary-f7a77"
-      )
-    ): await Firebase.initializeApp();
-  runApp( MyApp());
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: FirebaseOptions(
+              apiKey: "AIzaSyD_I8LTksveghTmTCDbAB8YHX8j0MCIryI",
+              appId: "1:714749532013:android:175644c4d9e4237d6cac64",
+              messagingSenderId: "714749532013",
+              projectId: "english-vocabulary-f7a77"))
+      : await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context,snapshot) {
-            if (snapshot.hasData) {
-              return mainPage();
-            }else {
-              return LoginScreen();
-            }
-          },
-        ),
-        theme: AppTheme.lightTheme,
-        darkTheme: AppTheme.darkTheme,
-        themeMode: ThemeMode.system,
-
-        routes: {
-          "/Homepage":(context) => HomeScreen(),
-          "/Setting" :(context) => Setting(),
-          "/Translator" :(context) => TranslatorScreen(),
-          "/Dictionary" :(context) => SearchPage(),
+      debugShowCheckedModeBanner: false,
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return HomeScreen();
+          } else {
+            return LoginScreen();
+          }
         },
-
+      ),
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      routes: {
+        "/Homepage": (context) => HomeScreen(),
+        "/Setting": (context) => Setting(),
+        "/Translator": (context) => TranslatorScreen(),
+        "/Dictionary": (context) => SearchPage(),
+        "/History": (context) => History(),
+      },
     );
   }
 }
