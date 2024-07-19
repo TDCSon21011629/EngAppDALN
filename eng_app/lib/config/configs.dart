881c861a6d5fa91fdde6a05321d6d723d0de903a
TDCSon <97428357+TDCSon21011629@users.noreply.github.com>
2024-07-19 16:25:14 +0700

    Add files via upload

---
+++ eng_app/lib/config/configs.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = 0xffff4f18;
+const kDismissColor = Colors.red;
+const kLightBlack = Colors.black12;
+
+const kSmallTextStyle = TextStyle(
+  fontFamily: 'Montserrat',
+  fontWeight: FontWeight.w400,
+  fontSize: 14.0,
+  color: Color(kPrimaryColor),
+);
+
+const kAppBarStyle = TextStyle(
+  fontFamily: 'Montserrat',
+  fontWeight: FontWeight.w700,
+  fontSize: 32.0,
+);
+
+const kLargeTextStyle = TextStyle(
+  fontFamily: 'Montserrat',
+  fontWeight: FontWeight.w600,
+  fontSize: 20.0,
+  color: Color(kPrimaryColor),
+);
+
+const kCustomCardWordTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 17.0,
+  fontFamily: 'Montserrat',
+);
+
+const kBackgroundColor = Color(0xffbbd5d5);
+
+const kMeaningTextStyle = TextStyle(
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+    color: Colors.black);
+
+const kCardTextStyle = TextStyle(
+  fontFamily: 'Montserrat',
+  fontSize: 24.0,
+  fontWeight: FontWeight.w600,
+  color: Color(kPrimaryColor),
+);
