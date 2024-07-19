f02230bd9ff52acc91afeea38b72b9ba3fb7b8d8
TDCSon <97428357+TDCSon21011629@users.noreply.github.com>
2024-07-19 16:30:01 +0700

    Add files via upload

---
+++ eng_app/lib/screens/word-detail/related_words.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:eng_app/config/configs.dart';
+import 'package:eng_app/model/dictionary.dart';
+
+class RelatedWords extends StatelessWidget {
+  const RelatedWords({Key key, this.wordDetail}) : super(key: key);
+
+  final Dictionary wordDetail;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        if (wordDetail.synonynms.isNotEmpty)
+          WordCard(
+            wordList: wordDetail.synonynms.join(' , '),
+            wordType: 'Synonyms',
+          ),
+        if (wordDetail.antonyms.isNotEmpty)
+          WordCard(
+            wordList: wordDetail.antonyms.join(' , '),
+            wordType: 'Antonyms',
+          ),
+        if (wordDetail.rhymes.isNotEmpty)
+          WordCard(
+            wordList: wordDetail.rhymes.join(' , '),
+            wordType: 'Rhyming Words',
+          ),
+      ],
+    );
+  }
+}
+
+class WordCard extends StatelessWidget {
+  const WordCard({Key key, this.wordList, this.wordType}) : super(key: key);
+
+  final String wordList;
+  final String wordType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            wordType,
+            style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+                color: Theme.of(context).primaryColor),
+          ),
+          const SizedBox(
+            height: 16.0,
+          ),
+          if (wordList.isNotEmpty)
+            Text(
+              wordList,
+              style: kMeaningTextStyle,
+            ),
+          const SizedBox(
+            height: 16.0,
+          ),
+          const Divider(),
+        ],
+      ),
+    );
+  }
+}
